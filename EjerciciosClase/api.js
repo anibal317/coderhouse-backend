1406873dc8423f6f2479e433515bf758d44208cf
Jorge Sard├│n <anibal317@gmail.com>
2022-08-11 18:16:42 -0300

    Ejercicio en clase usando query params

---
+++ EjerciciosClase/api.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const frase = "Pariatur velit est duis qui aliquip dolore cillum in commodo consectetur esse excepteur ad tempor."
+
+app.get("/", (req, res) => {
+    // res.send(frase)
+    res.send("Bienvenido")
+})
+app.get("/api/frase", (req, res) => {
+    // res.send(frase)
+    res.send(req.query)
+})
+
+app.get("/api/letras/:num", (req, res) => {
+    let num = req.params.num
+    // console.log(frase.charAt(num))
+    res.send(frase[num])
+})
+
+app.get("/api/palabras/:num", (req, res) => {
+    let pos = req.params.num
+    res.send(frase.split(" ")[pos])
+});
+
+app.listen(8080, () => {
+    console.log('Servidor corriendo')
+})
