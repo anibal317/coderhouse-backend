4b3839f6010dc9869645e1c4bc6a34db129bc445
Jorge Sard├│n <anibal317@gmail.com>
2022-08-28 17:26:26 -0300

    WebSockets

---
+++ Entregable 6/js/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+const products = require('./api/products');
+
+const app = express();
+
+
+app.engine('handlebars', engine())
+app.set("view engine", "handlebars");
+app.set("views", "./views")
+
+app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/products', products);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('productos', { 
+        service1: "Alta de productos",
+        service2: "Alta de proveedores",
+        service3:"Seguimiento de pedidos"
+     })
+})
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado... http://localhost:3000');
+});
