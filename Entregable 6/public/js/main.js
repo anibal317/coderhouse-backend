4c4c8cc09939778f7831522a3313f38570465e7e
Jorge Sard├│n <anibal317@gmail.com>
2022-08-31 20:13:10 -0300

    creando formulario para el chat

--- Entregable 6/public/js/main.js
+++ Entregable 6/public/js/main.js
@@ -1,7 +1,12 @@
+const { text } = require("express")
+
 const socket = io.connect()
 
 const inputMessage = document.getElementById('textAreaExample')
 const senderMessage = document.getElementById('senderMessage')
+const author = document.getElementById('userName')
+const text = document.getElementById('textAreaTexto')
+
 
 inputMessage.addEventListener('input', () => {
     socket.emit("mensajeEnviado", inputMessage.value)
@@ -11,3 +16,12 @@ inputMessage.addEventListener('input', () => {
 socket.on('mensajesRecibidos', (mensajes) => {
     senderMessage.innerText = mensajes
 })
+
+function addMessage(e){
+    const message = {
+        author: author.value,
+        text: text.value
+    }
+    socket.edit('New-message', message)
+    return false
+}
